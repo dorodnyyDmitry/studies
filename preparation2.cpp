2d34cdc08d37ae31c2546ac73cfd9206b7534b1c
betLomo <betta.po@yandex.ru>
2016-06-15 20:56:35 +0300

    Create preparation2.cpp

---
+++ preparation2.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	string g; //вспомогательная строка для потоков
+	getline(cin, s);
+	while (s.find("  ") != -1) //удаление лишних пробелов
+	{
+		s.erase(s.find("  "), 1);
+	}
+	if (s[0] == ' ') //удаление первого пробела
+		s.erase(0, 1);
+	if (s[s.size() - 1] == ' ') //удаление последнего пробела
+		s.erase(s.size() - 1, 1);
+	//cout << s;
+
+	istringstream Potochek(s);
+	istringstream quantity(s);
+
+	int wordQuantity = 0;
+	int lastWordLength = 999999999;
+	int proof=0;
+
+	while(quantity >> g)
+	{
+		wordQuantity++;
+	}
+
+	while (Potochek >> g)
+	{
+		if (g.size() < lastWordLength)
+			proof++;
+		lastWordLength = g.size();
+	}
+
+	if (proof == wordQuantity)
+		cout << "YES";
+	else cout << "NO";
+	return 0;
+}
